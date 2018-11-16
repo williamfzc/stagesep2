27fc966492e57e0e76538f8dc5006855b1a15697
williamfzc <178894043@qq.com>
2018-11-16 19:21:41 +0800

    building analyser

--- stagesep2/analyser.py
+++ stagesep2/analyser.py
@@ -4,20 +4,29 @@
 - OCR
 - 特征识别
 """
+import contextlib
+import cv2
+
 from stagesep2.loader import VideoManager
 from stagesep2.config import Config
 
 
-class OCRAnalyser(object):
+class BaseAnalyser(object):
+    name = ''
+
     @classmethod
-    def run(cls):
-        pass
+    def run(cls, frame):
+        return ''
 
 
-class MatchTemplateAnalyser(object):
-    @classmethod
-    def run(cls):
-        pass
+class OCRAnalyser(BaseAnalyser):
+    """ ocr analyser """
+    name = 'OCR'
+
+
+class MatchTemplateAnalyser(BaseAnalyser):
+    """ match-template analyser """
+    name = 'match_template'
 
 
 ANALYSER_DICT = {
@@ -27,12 +36,21 @@ ANALYSER_DICT = {
 
 
 def check_analyser(analyser_list):
+    """ check if analyser existed, and return list of runnable analyser """
     new_analyser_list = list()
     for each in analyser_list:
         if each not in ANALYSER_DICT:
             raise NotImplementedError('analyser {} not found'.format(each))
         new_analyser_list.append(ANALYSER_DICT[each])
     return new_analyser_list
+
+
+@contextlib.contextmanager
+def video_capture(ssv):
+    """ 打开视频的上下文控制 """
+    video_cap = cv2.VideoCapture(ssv.video_path)
+    yield video_cap
+    video_cap.release()
 
 
 class AnalyserRunner(object):
@@ -53,5 +71,15 @@ class AnalyserRunner(object):
         analyser_list = check_analyser(Config.analyser_list)
         video_dict = VideoManager.video_dict
 
-        for each_video_name, each_video in video_dict.items():
-            pass
+        for each_video_name, each_ssv in video_dict.items():
+            with video_capture(each_ssv) as each_video:
+                ret, frame = each_video.read()
+                while ret:
+                    if not ret:
+                        # end of video
+                        break
+                    # TODO main logic
+                    result_dict = dict()
+                    for each_analyser in analyser_list:
+                        result = each_analyser.run(frame)
+                        result_dict[each_analyser.name] = result
