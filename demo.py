4f99b3d57e8c5664342d4d63a228a23baa3670ac
William Feng <178894043@qq.com>
2018-11-16 17:29:17 +0800

    modify demo

--- demo.py
+++ demo.py
@@ -4,12 +4,11 @@ from stagesep2.analyser import AnalyserRunner
 from stagesep2.config import Config
 
 
-video1 = VideoManager.add('some_path/video1.mp4')
-video2 = VideoManager.add('some_path/video2.mp4')
+video = VideoManager.add('some_path/video1.mp4')
 
 # optional
-video1.template_manager.add('some_path/template1.png')
-video2.template_manager.add('some_path/template2.png')
+video.template_manager.add('some_path/template1.png')
+video.template_manager.add('some_path/template2.png')
 
 # change config
 Config.analyser_list = ['ocr', 'match_template']
