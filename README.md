f176cf2dd982ea78b8777bbd9c3cf3cfe9078f1b
williamfzc <178894043@qq.com>
2018-12-04 15:31:50 +0800

    more friendly

--- README.md
+++ README.md
@@ -10,9 +10,9 @@
 
 - 虽然它现在运作尚未发现大问题，但是很大程度上限制了可维护性与后续开发的可能
 - 带有零碎的内容过多，而实际上它们可能作用不大
-- 在API的设计上不够合理
+- 在API的设计上不够合理，难以应用到真实工程中
 
-针对上述问题，对整个工程进行了重构，删繁就简。
+针对上述问题，对整个工程进行了重构，删繁就简。现在已经真正落地到现实项目中。
 
 ## 使用
 
@@ -22,29 +22,18 @@
 
 ```pip install stagesep2```
 
-### 例子
-
-主要支持 OCR / 模板匹配 / 首尾帧相似度。
-
-提供了用户友好的API供用户调用：
+### 简单例子
 
 ```python
 # :)
 from stagesep2.loader import VideoManager
 from stagesep2.executor import AnalysisRunner
-from stagesep2.config import NormalConfig
 
 
 video = VideoManager.add('./demo_video.mp4')
 
-# 逆时针旋转 90 * rotate 度
-video.rotate = 3
-
-# optional
+# add template picture
 video.template_manager.add('./template1.png')
-
-# change config
-NormalConfig.ANALYSER_LIST = ['ocr', 'match_template', 'trend']
 
 # start
 result = AnalysisRunner.run()
@@ -56,7 +45,7 @@ dict_data = result.data
 result.export('./result.json')
 ```
 
-更多使用方式见[demo.py](demo.py)。
+更多使用方式，推荐看[demo.py](demo.py)。
 
 ## 原理
 
--- demo.py
+++ demo.py
@@ -1,23 +1,35 @@
 # :)
-from stagesep2 import VideoManager, AnalysisRunner, NormalConfig
+from stagesep2 import VideoManager, AnalysisRunner, NormalConfig, OCRConfig
 
 
+# 添加待测视频
+# 虽然能够支持多视频，但还是建议每次只分析一个
 video = VideoManager.add('./temp.avi')
 
+# 旋转视频
+# 因为拍摄设备的差异，拍摄的视频可能方向是不同预期的，容易导致分析器分析不出有效结果
+# 设定该参数能够旋转视频，使分析器可以正常生效
 # 逆时针旋转 90 * rotate 度
 video.rotate = 3
 
-# optional
+# 添加match template的样本图
 video.template_manager.add('./template.png')
 
-# change config
+# 分析器列表
+# 默认情况下会全选，使用 OCR/模型匹配/首尾帧相似度 进行处理
 NormalConfig.ANALYSER_LIST = ['ocr', 'match_template', 'trend']
 
-# start
+# 修改OCR的语种
+# 默认情况下是英文（tesseract自带了英文）
+# 如果使用中文，则需要自行安装tesseract的中文支持包，详见tesseract wiki：
+# https://github.com/tesseract-ocr/tesseract/wiki
+OCRConfig.lang = 'chi_sim'
+
+# 启动分析
 result = AnalysisRunner.run()
 
-# analyse it (is a dict)
+# 分析的结果（dict）
 dict_data = result.data
 
-# or export your result
+# 或者输出到文件内（json）
 result.export('./result.json')
