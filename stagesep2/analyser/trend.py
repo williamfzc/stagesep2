8ebb81078095e0c00c5b15b099b674bd4a9e0f71
williamfzc <178894043@qq.com>
2018-12-05 14:54:08 +0800

    add previous frame comparison

--- stagesep2/analyser/trend.py
+++ stagesep2/analyser/trend.py
@@ -6,19 +6,31 @@ from stagesep2.analyser.base import BaseAnalyser
 class TrendAnalyser(BaseAnalyser):
     """ trend analyser """
     name = 'trend'
+    previous_frame = None
 
     @classmethod
     def run(cls, frame, ssv):
         """
-        return a dict, eg: {'first': 0.9, 'last': 0.2}
+        return a dict, eg: {'previous': 1.0, 'first': 0.9, 'last': 0.2}
 
         :param frame:
         :param ssv:
         :return:
         """
+        # init previous frame
+        if cls.previous_frame is None:
+            cls.previous_frame = frame
+
+        # calculate sim
+        previous_sim = compare_ssim(cls.previous_frame, frame)
         first_sim = compare_ssim(ssv.first_frame, frame)
         last_sim = compare_ssim(ssv.last_frame, frame)
+
+        # update previous frame
+        cls.previous_frame = frame
+
         result_dict = {
+            'previous': previous_sim,
             'first': first_sim,
             'last': last_sim,
         }
