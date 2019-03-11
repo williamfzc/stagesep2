0663cfd31413670876f0834457b4fdfe91a897f6
williamfzc <178894043@qq.com>
2019-03-11 16:58:56 +0800

    fix: bug when using more than one template

--- stagesep2/analyser/match_template.py
+++ stagesep2/analyser/match_template.py
@@ -1,8 +1,6 @@
 from stagesep2.analyser.base import BaseAnalyser
 from findit import FindIt
 
-fi = FindIt()
-
 
 class MatchTemplateAnalyser(BaseAnalyser):
     """ match-template analyser """
@@ -12,7 +10,9 @@ class MatchTemplateAnalyser(BaseAnalyser):
     def run(cls, frame, ssv):
         result_dict = dict()
         template_pic_dict = ssv.template_manager.get_dict()
+
         for each_pic_name, each_pic in template_pic_dict.items():
+            fi = FindIt()
             fi.template[each_pic.pic_path] = each_pic.cv_object
             match_result = fi.find(target_cv_object=frame)['data'][0]
             max_val, min_val = match_result['max_val'], match_result['min_val']
