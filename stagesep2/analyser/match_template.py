8b26eccb8fd1f897b270cb928ed49693ecadb8fc
williamfzc <178894043@qq.com>
2019-03-14 14:50:14 +0800

    fix: sync with latest findit

--- stagesep2/analyser/match_template.py
+++ stagesep2/analyser/match_template.py
@@ -1,5 +1,7 @@
 from stagesep2.analyser.base import BaseAnalyser
+from stagesep2.config import MatchTemplateConfig
 from findit import FindIt
+import cv2
 
 
 class MatchTemplateAnalyser(BaseAnalyser):
@@ -8,11 +10,17 @@ class MatchTemplateAnalyser(BaseAnalyser):
 
     @classmethod
     def run(cls, frame, ssv):
+        # check conf
+        assert MatchTemplateConfig.cv_method not in (cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED), 'SQDIFF NOT SUPPORT'
+
+        # init
+        fi = FindIt()
+        fi.config.cv_method = MatchTemplateConfig.cv_method
+
         result_dict = dict()
         template_pic_dict = ssv.template_manager.get_dict()
 
         for each_pic_name, each_pic in template_pic_dict.items():
-            fi = FindIt()
             fi.template[each_pic.pic_path] = each_pic.cv_object
             match_result = fi.find(target_cv_object=frame)['data'][0]
             max_val, min_val = match_result['max_val'], match_result['min_val']
