9c6bedb54605c3e37a0c702666be3a2fff47c4a7
williamfzc <178894043@qq.com>
2019-05-29 17:09:18 +0800

    chore: some refactor with findit

--- stagesep2/analyser/match_template.py
+++ stagesep2/analyser/match_template.py
@@ -14,18 +14,22 @@ class MatchTemplateAnalyser(BaseAnalyser):
 
     @classmethod
     def run(cls, frame, ssv):
-        result_dict = dict()
         template_pic_dict = ssv.template_manager.get_dict()
-
         for each_pic_name, each_pic in template_pic_dict.items():
             fi.load_template(each_pic_name, pic_object=each_pic.cv_object)
-            match_result = list(fi.find('temp', target_pic_object=frame)['data'].values())[0]['TemplateEngine']
-            max_val = match_result['target_sim']
 
+        match_result = fi.find(
+            'temp',
+            target_pic_object=frame,
+            engine_template_scale=(1, 2, 5)
+        )['data']
+
+        result_dict = dict()
+        for each_pic_name, each_pic_result in match_result.items():
             result_dict[each_pic_name] = {
                 # no min any more
                 'min': -1,
-                'max': max_val,
+                'max': each_pic_result['TemplateEngine']['target_sim'],
             }
-            fi.clear()
+        fi.clear()
         return result_dict
