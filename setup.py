d82724e68376974c70c455e2f2d4653e4fb59c44
williamfzc <178894043@qq.com>
2019-06-12 19:06:01 +0800

    release: 0.2.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='stagesep2',
-    version='0.2.5',
+    version='0.2.6',
     description='Analyse, and convert video into useful data.',
     author='williamfzc',
     author_email='fengzc@vip.qq.com',
